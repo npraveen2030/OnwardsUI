0f44dfd7573d2e13a0e5d322d6759229b1325956
npraveen2030 <npraveen2030@gmail.com>
2025-07-14 21:25:21 +0530

    Added styles

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,50 @@
-import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
 
-import { routes } from './app.routes';
+// src/app/app.config.ts
+import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+import { provideRouter, Routes } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
+  
+import { MainLayoutComponent } from './layout/main-layout/main-layout.component';
+import { Login } from './pages/login/login';
+import { Reports } from './pages/reports/reports';
+import { Dashboard } from './pages/dashboard/dashboard';
+import { Register } from './pages/register/register';
+
+const routes: Routes = [
+  { path: 'login', component: Login },
+  { path: '', redirectTo: 'login', pathMatch: 'full' },  // optional redirect
+  {
+    path: '',
+    component: MainLayoutComponent,
+    children: [
+      { path: 'report', component: Reports },
+      { path: 'dashboard', component: Dashboard },
+      { path: 'register', component: Register }
+    ]
+  },
+  { path: '**', redirectTo: 'login' } // catch-all
+];
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideBrowserGlobalErrorListeners(),
+        provideBrowserGlobalErrorListeners(),
     provideZonelessChangeDetection(),
     provideHttpClient(),
     provideRouter(routes)
   ]
 };
+ 
+// import { ApplicationConfig, provideBrowserGlobalErrorListeners, provideZonelessChangeDetection } from '@angular/core';
+// import { provideRouter } from '@angular/router';
+
+// import { routes } from './app.routes';
+// import { provideHttpClient } from '@angular/common/http';
+
+// export const appConfig: ApplicationConfig = {
+//   providers: [
+//     provideBrowserGlobalErrorListeners(),
+//     provideZonelessChangeDetection(),
+//     provideHttpClient(),
+//     provideRouter(routes)
+//   ]
+// };
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,20 +1,23 @@
-import { Routes } from '@angular/router';
-import { Dashboard } from './pages/dashboard/dashboard';
-import { Orders } from './pages/orders/orders';
-import { Reports } from './pages/reports/reports';
-import { Clients } from './pages/clients/clients';
-import { Register } from './pages/register/register';
-import { Login} from './pages/login/login';
-import { Logout } from './pages/logout/logout';
+// import { Routes } from '@angular/router';
+// import { Dashboard } from './pages/dashboard/dashboard';
+// import { Orders } from './pages/orders/orders';
+// import { Reports } from './pages/reports/reports';
+// import { Clients } from './pages/clients/clients';
+// import { Register } from './pages/register/register';
+// import { Login} from './pages/login/login';
+// import { Logout } from './pages/logout/logout';
 
-export const routes: Routes = [
-  { path: '', redirectTo: 'login', pathMatch: 'full' },
-  {path: 'login', component: Login},
-  { path: 'dashboard', component: Dashboard },
-  { path: 'orders', component: Orders },
-  { path: 'reports', component: Reports },
-  { path: 'clients', component: Clients },
-  { path: 'register', component: Register },
-  { path: 'logout', component: Logout},
-  { path: '**', redirectTo: 'login' }
-];
+// export const routes: Routes = [
+//   { path: '', redirectTo: 'login', pathMatch: 'full' },
+//   {path: 'login', component: Login},
+//   { path: 'dashboard', component: Dashboard },
+//   { path: 'orders', component: Orders },
+//   { path: 'reports', component: Reports },
+//   { path: 'clients', component: Clients },
+//   { path: 'register', component: Register },
+//   { path: 'logout', component: Logout},
+//   { path: '**', redirectTo: 'login' }
+// ];
+import { Routes } from '@angular/router';
+
+export const routes: Routes = [];
